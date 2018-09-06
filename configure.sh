9fc1bf28e9546fcdf7d4bd0eb8a8125ac83693de
Krzysztof Krysiak <krysiak86@gmail.com>
2018-09-06 16:10:44 +0200

    Add .zshrc file

--- configure.sh
+++ configure.sh
@@ -6,13 +6,14 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # My config files configuration
 git clone https://github.com/kajstof/dotfiles ~/dotfiles
-rm -rf ~/.vimrc ~/.Xresources ~/.config/fontconfig ~/.config/i3 ~/.config/rofi ~/.config/termite
+rm -rf ~/.vimrc ~/.Xresources ~/.config/fontconfig ~/.config/i3 ~/.config/rofi ~/.config/termite ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.Xresources ~/.Xresources
 ln -s ~/dotfiles/.config/fontconfig ~/.config/fontconfig
 ln -s ~/dotfiles/.config/i3 ~/.config/i3
 ln -s ~/dotfiles/.config/rofi ~/.config/rofi
 ln -s ~/dotfiles/.config/termite ~/.config/termite
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 
 # Vim plugins installation
 vim +PluginInstall +qall
