c8550cc2525469572f2d5d4444fb6bbcb086f099
Krzysztof Krysiak <krysiak86@gmail.com>
2019-01-02 20:30:55 +0100

    Add more modules to Polybar installation script

--- install.sh
+++ install.sh
@@ -33,11 +33,19 @@ rm ~/Tools/i3-gaps/ -rf
 
 # Polybar
 cd ~/Tools
-git clone --branch 3.2 --recursive https://github.com/jaagr/polybar
+sudo apt install -y build-essential git cmake cmake-data pkg-config libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+sudo apt install -y unifont libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev 
+git clone https://github.com/stark/siji && cd siji
+./install.sh -d ~/.fonts
+cd ..
+rm siji -rf
+git clone --recursive https://github.com/jaagr/polybar
 mkdir polybar/build
 cd polybar/build
 cmake ..
 sudo make install
+cd ~
+rm Tools/polybar -rf
 
 # Google Chrome
 cd ~/Downloads
