0177f0247ebc97afa7143cb9cf131388c21257fa
Krzysztof Krysiak <krysiak86@gmail.com>
2023-01-13 16:56:23 +0100

    Update configuration

--- scripts/polybar_launch.sh
+++ scripts/polybar_launch.sh
@@ -11,7 +11,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "polybar"; then
   count=0
-  for m in $(polybar -m | cut -d":" -f1 | tac); do
+  # for m in $(polybar -m | cut -d":" -f1 | tac); do
+  for m in $(polybar -m | cut -d":" -f1); do
     export MONITOR="$m"
     if [ $count -gt 0 ]; then
       profile="secondary"
